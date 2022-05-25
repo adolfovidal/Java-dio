217583c08cea5baf01a25a29c3d7bb46311bc157
adolfovidal <adolfosvidal@gmail.com>
2022-05-25 12:40:02 -0300

    POO Java

---
+++ Estudos/java/Atividades-POO/src/dio/Carro.java
@@ -0,0 +1,45 @@
+package dio;
+
+public class Carro {
+
+    String cor;
+    String modelo;
+    int capacidadeTanque;
+
+    Carro() {
+
+    }
+    Carro (String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getcapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorcombustivel) {
+        return capacidadeTanque * valorcombustivel;
+    }
+}
